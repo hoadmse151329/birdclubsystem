8a54a9cabb0e5bf4275c1179849f0a9176fd1761
Edwin Bùi Hữu Vinh <bhvinh.brv@gmail.com>
2023-12-07 16:03:38 +0700

    Add BusinessObject v0.1

---
+++ BusinessObject/Models/Users.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject.Models
+{
+    public partial class Users
+    {
+        public string userId { get; set; } = null!;
+        public string? clubId { get; set; }
+        public string? userName { get; set; }
+        public string? password { get; set; }
+        public string? memberId { get; set; }
+    }
+}
