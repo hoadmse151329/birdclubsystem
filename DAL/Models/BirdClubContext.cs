afe40a2740764eb6a5939c1e6ceca12b95e15e49
Echo2craft <justechojepar@gmail.com>
2023-12-13 16:45:09 +0700

    change appsettings

--- DAL/Models/BirdClubContext.cs
+++ DAL/Models/BirdClubContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
 
 namespace DAL.Models
 {
@@ -49,8 +50,12 @@ namespace DAL.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
+                IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+                string connectionString = config["ConnectionStrings:DefaultConnection"];
+                optionsBuilder.UseSqlServer(connectionString);
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=LAPTOP-VR0KVDVV\\SQLEXPRESS;Initial Catalog=BirdClub;Integrated Security=True");
+                //optionsBuilder.UseSqlServer("Data Source=LAPTOP-VR0KVDVV\\SQLEXPRESS;Initial Catalog=BirdClub;Integrated Security=True");
             }
         }
 
