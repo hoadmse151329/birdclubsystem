cfcaccbccd024e435c8fb7f09b2a11b64eb5df23
DuyZomega <82254739+DuyZomega@users.noreply.github.com>
2023-12-14 13:12:03 +0700

    update
    update

---
+++ DAL/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,48 @@
+﻿using DAL.Models;
+using DAL.Repositories.Implements;
+using DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Models;
+using DAL.Repositories.Implements;
+using DAL.Repositories.Interfaces;
+
+namespace DAL.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly BirdClubContext _context;
+        private IMeetingRepository _meetingRepository;
+        private IMeetingMediaRepository _meetingMediaRepository;
+        private IMeetingParticipantRepository _meetingParticipantRepository;
+        private IMemberRepository _memberRepository;
+        private IUserRepository _userRepository;
+        public IUserRepository UserRepository => _userRepository ??= new UserRepository(_context);
+
+        public IMeetingRepository MeetingRepository => throw new NotImplementedException();
+
+        public IMeetingMediaRepository MediaRepository => throw new NotImplementedException();
+
+        public IMeetingParticipantRepository ParticipantRepository => throw new NotImplementedException();
+
+        public IMemberRepository MemberRepository => throw new NotImplementedException();
+
+        public UnitOfWork(BirdClubContext context)
+        {
+            _context = context;
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
