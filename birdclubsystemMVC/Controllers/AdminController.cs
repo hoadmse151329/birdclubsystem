605f8ac3bdf0bbe05c57a7fa2763bbc813f7490d
Edwin Bùi Hữu Vinh <bhvinh.brv@gmail.com>
2023-12-20 17:28:26 +0700

    Add some controllers

---
+++ birdclubsystemMVC/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace birdclubsystem.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Admin()
+        {
+            return View();
+        }
+    }
+}
