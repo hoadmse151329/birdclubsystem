defcc315f455c4f18ce577e70eb77cc381605d58
Edwin Bùi Hữu Vinh <bhvinh.brv@gmail.com>
2023-12-23 09:08:51 +0700

    add create and update function in meeting controller api

--- WebAPI/Controllers/MeetingController.cs
+++ WebAPI/Controllers/MeetingController.cs
@@ -1,6 +1,7 @@
 ﻿using BAL.Services.Implements;
 using BAL.Services.Interfaces;
 using BAL.ViewModels;
+using DAL.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -39,6 +40,85 @@ namespace WebAPI.Controllers
                     });
                 }
 
+                return Ok(new
+                {
+                    status = true,
+                    result
+                });
+            }
+            catch (Exception ex)
+            {
+                // Log the exception if needed
+                return BadRequest(new
+                {
+                    status = false,
+                    errorMessage = ex.Message
+                });
+            }
+        }
+
+        [HttpPost("Create")]
+        [Authorize(Roles = "Manager")]
+        public IActionResult Create (string meetingName, string description, DateTime registrationDeadline, DateTime startDate, DateTime endDate, int numberOfParticipants, string host, string incharge, string note)
+        {
+            try
+            {
+                MeetingViewModel value = new MeetingViewModel
+                {
+                    MeetingName = meetingName,
+                    Description = description,
+                    RegistrationDeadline = registrationDeadline,
+                    StartDate = startDate,
+                    EndDate = endDate,
+                    NumberOfParticipants = numberOfParticipants,
+                    Host = host,
+                    Incharge = incharge,
+                    Note = note
+                };
+                _meetingService.Create(value);
+                return Ok(new
+                {
+                    status = true,
+                    Message = "Meeting Create successfully !",
+                    value
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new
+                {
+                    status = false,
+                    errorMessage = ex.Message
+                });
+            }
+        }
+
+        [HttpPut("Update")]
+        [Authorize(Roles = "Manager")]
+        public IActionResult Update(int id, string meetingname, string description, DateTime registrationDeadline, DateTime startDate, DateTime endDate, int numberOfParticipants, string host, string incharge, string note)
+        {
+            try
+            {
+                var result = _meetingService.GetById(id);
+                if (result == null)
+                {
+                    return NotFound(new
+                    {
+                        status = false,
+                        errorMessage = "Meeting does not exist!"
+                    });
+                }
+                result.MeetingName = meetingname;
+                result.Description = description;
+                result.RegistrationDeadline = registrationDeadline;
+                result.StartDate = startDate;
+                result.EndDate = endDate;
+                result.NumberOfParticipants = numberOfParticipants;
+                result.Host = host;
+                result.Incharge = incharge;
+                result.Note = note;
+                _meetingService.Update(result);
+                result = _meetingService.GetById(id);
                 return Ok(new
                 {
                     status = true,
