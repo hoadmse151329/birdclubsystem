c465c03878cea908cbdd1e4b106cf7cf04cbab6d
DuyZomega <82254739+DuyZomega@users.noreply.github.com>
2023-12-26 09:15:16 +0700

    update meeting

--- birdclubsystemMVC/Controllers/MeetingController.cs
+++ birdclubsystemMVC/Controllers/MeetingController.cs
@@ -1,15 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.VisualBasic;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Text;
 namespace birdclubsystem.Controllers
 {
     public class MeetingController : Controller
     {
 		private readonly ILogger<MeetingController> _logger;
-
-		public MeetingController(ILogger<MeetingController> logger)
-		{
-			_logger = logger;
-		}
+        private readonly HttpClient _httpClient;
+        private string MeetingAPI_URL = "";
+        public MeetingController(ILogger<MeetingController> logger, IHttpClientFactory httpClientFactory)
+        {
+            _logger = logger;
+            _httpClient = httpClientFactory.CreateClient();
+            var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+            _httpClient.DefaultRequestHeaders.Accept.Add(contentType);
+            _httpClient.BaseAddress = new Uri("https://localhost:7022");
+            MeetingAPI_URL = "/api/Meeting";
+        }
 		public IActionResult Index()
         {
             return View();
