79d53c3ef53920b0bc4acd454d9decfc4214d1b6
Echo2craft <justechojepar@gmail.com>
2023-12-27 21:04:45 +0700

    Rename birdclubsystemMVC to WebAppMVC

--- WebAPI/Controllers/MeetingController.cs
+++ WebAPI/Controllers/MeetingController.cs
@@ -22,7 +22,7 @@ namespace WebAPI.Controllers
         }
 
         [HttpGet("{id}")]
-        [Authorize(Roles = "Manager")]
+        [Authorize(Roles = "Manager,Member")]
         [ProducesResponseType(typeof(MeetingViewModel), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
