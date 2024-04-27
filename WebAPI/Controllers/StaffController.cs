f635c799235a7c7e8509e3641b4ca9027a4bec55
Echo2craft <justechojepar@gmail>
2024-04-27 16:58:19 +0700

    Temporary fix Staff Pages

--- WebAPI/Controllers/StaffController.cs
+++ WebAPI/Controllers/StaffController.cs
@@ -78,6 +78,50 @@ namespace WebAPI.Controllers
             }
         }
 
+        [HttpPost("Profile")]
+        [Authorize(Roles = "Staff")]
+        [ProducesResponseType(typeof(MemberViewModel), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> GetStaffDetailsByUsrId([FromBody] string memId)
+        {
+            try
+            {
+                var result = await _memberService.GetById(memId);
+                if (result == null)
+                {
+                    return NotFound(new
+                    {
+                        Status = false,
+                        ErrorMessage = "Staff Details Not Found!"
+                    });
+                }
+                return Ok(new
+                {
+                    Status = true,
+                    Data = result
+                });
+            }
+            catch (Exception ex)
+            {
+                if (ex.InnerException != null)
+                {
+                    return BadRequest(new
+                    {
+                        Status = false,
+                        ErrorMessage = ex.Message,
+                        InnerExceptionMessage = ex.InnerException.Message
+                    });
+                }
+                // Log the exception if needed
+                return BadRequest(new
+                {
+                    Status = false,
+                    ErrorMessage = ex.Message
+                });
+            }
+        }
+
         [HttpPut("MeetingParticipantStatus/Update/{id:int}")]
         [Authorize(Roles = "Staff")]
         [ProducesResponseType(typeof(IEnumerable<MeetingParticipantViewModel>), StatusCodes.Status200OK)]
