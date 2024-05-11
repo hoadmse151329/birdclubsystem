b05e3f7fca7f76b6b79dbf160d51c220107f27a0
DuyZomega <82254739+DuyZomega@users.noreply.github.com>
2024-05-11 15:26:44 +0700

    up google account

--- WebAppMVC/Controllers/AuthController.cs
+++ WebAppMVC/Controllers/AuthController.cs
@@ -11,6 +11,9 @@ using WebAppMVC.Services;
 using WebAppMVC.Models.VnPay;
 using WebAppMVC.Models.Transaction;
 using BAL.ViewModels;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
 
 namespace WebAppMVC.Controllers
 {
@@ -68,36 +71,32 @@ namespace WebAppMVC.Controllers
             return View();
 		}
 
-        #region Old Google Login Code (Deprecated)
-        /*public async Task GoogleLogin()
+		#region Old Google Login Code (Deprecated)
+		[HttpGet("GoogleLogin")]
+		public IActionResult GoogleLogin()
 		{
-			await HttpContext.ChallengeAsync(GoogleDefaults.AuthenticationScheme,
-				new AuthenticationProperties
-				{
-					RedirectUri = Url.Action("GoogleResponse")
-				});
+			var properties = new AuthenticationProperties
+			{
+				RedirectUri = Url.Action(nameof(GoogleResponse))
+			};
+			return Challenge(properties, GoogleDefaults.AuthenticationScheme);
 		}
+		[HttpGet("GoogleResponse")]
 		public async Task<IActionResult> GoogleResponse()
 		{
 			var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-			
-			var claims = result.Principal.Identities.FirstOrDefault().Claims.Select(claim => new
+
+			if (result?.Succeeded != true)
 			{
-				claim.Issuer,
-				claim.OriginalIssuer,
-				claim.Type,
-				claim.Value
-			});
-			*//*if (result.Succeeded)
-			{
-				
+				// Handle failed authentication
+				return RedirectToAction(nameof(Login));
 			}
+
 			var authenResponse = await methcall.CallMethodReturnObject<GetAuthenResponse>(
 				_httpClient: client,
 				options: jsonOptions,
 				methodName: "POST",
 				url: AuthenAPI_URL,
-				inputType: newmemRequest,
 				_logger: _logger);
 
 			if (authenResponse == null)
@@ -114,36 +113,39 @@ namespace WebAppMVC.Controllers
 				HttpContext.Session.SetString("ACCESS_TOKEN", responseAuth.AccessToken);
 				HttpContext.Session.SetString("ROLE_NAME", responseAuth.RoleName);
 				HttpContext.Session.SetString("USER_ID", responseAuth.UserId);
+				HttpContext.Session.SetString("USER_NAME", responseAuth.UserName);
+				HttpContext.Session.SetString("IMAGE_PATH", responseAuth.ImagePath);
 				client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", responseAuth.AccessToken);
+
 				TempData["ACCESS_TOKEN"] = responseAuth.AccessToken;
 				TempData["ROLE_NAME"] = responseAuth.RoleName;
 				TempData["USER_ID"] = responseAuth.UserId;
+				TempData["USER_NAME"] = responseAuth.UserName;
+				TempData["IMAGE_PATH"] = responseAuth.ImagePath;
 			}
 			if (responseAuth!.RoleName == Constants.Constants.ADMIN)
 			{
-				_logger.LogInformation("Admin Register Successful: " + TempData["ROLE_NAME"] + " , Id: " + TempData["USER_ID"]);
+				_logger.LogInformation("Admin Login Successful: " + TempData["ROLE_NAME"] + " , Id: " + TempData["USER_ID"]);
 				return base.Redirect(Constants.Constants.ADMIN_URL);
 			}
 			else if (responseAuth!.RoleName == Constants.Constants.MANAGER)
 			{
-				_logger.LogInformation("Manager Register Successful: " + TempData["ROLE_NAME"] + " , Id: " + TempData["USER_ID"]);
+				_logger.LogInformation("Manager Login Successful: " + TempData["ROLE_NAME"] + " , Id: " + TempData["USER_ID"]);
 				return base.Redirect(Constants.Constants.MANAGER_URL);
 			}
 			else if (responseAuth!.RoleName == Constants.Constants.STAFF)
 			{
-				_logger.LogInformation("Staff Register Successful: " + TempData["ROLE_NAME"] + " , Id: " + TempData["USER_ID"]);
+				_logger.LogInformation("Staff Login Successful: " + TempData["ROLE_NAME"] + " , Id: " + TempData["USER_ID"]);
 				return base.Redirect(Constants.Constants.STAFF_URL);
 			}
 			else
 			{
-				_logger.LogInformation("Member Register Successful: " + TempData["ROLE_NAME"] + " , Id: " + TempData["USER_ID"]);
+				_logger.LogInformation("Member Login Successful: " + TempData["ROLE_NAME"] + " , Id: " + TempData["USER_ID"]);
 				return base.Redirect(Constants.Constants.MEMBER_URL);
-			}*//*
-			return base.Redirect(Constants.Constants.MEMBER_URL);
-		}*/
-        #endregion
-
-        [HttpGet("Logout")]
+			}
+		}
+			#endregion
+			[HttpGet("Logout")]
         public IActionResult Logout()
         {
 			client.DefaultRequestHeaders.Authorization = null;
